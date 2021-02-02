091e14cc72502dcefcb1c160ac94fa9552155013
Mirla Montano <mirmon.exe@outlook.com>
2021-02-01 23:07:55 -0400

    Procedimientos Almacenados Pacientes
    creación de sp de la tabla Pacientes

---
+++ CrystalClear/Datos/Conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    class Conexion 
+    {
+
+        //Aqui se establece la conexion con la DB
+
+        public static string CadenaConexion = "Data Source=MIRLU\\SQLEXPRESS; Initial Catalog=CrystalClearBD; Integrate Security=true";
+
+
+        //datasource laptop mirlu MIRLU\\SQLEXPRESS
+    }
+}
