da5a4085c31ba545525c355bd3e0e84428786b72
Mirla Montano <mirmon.exe@outlook.com>
2021-02-09 16:27:30 -0400

    sustitui nombre de la ultima capa "Interfaz" por "Presentacion"

---
+++ CrystalClear/Metodos/NPacientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Datos;
+using System.Data;
+
+namespace Metodos
+{
+    public class NPacientes
+    {
+
+        //Metodo Insertar que llama al metodo Insertar de la clase DPaciente de la capa Datos
+
+        public static string Insertar(string nombre, );
+
+    }
+}
